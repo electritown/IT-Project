3a12d15501413c2a38d174f96c41ea99b4de8efe
electritown <abdelrhman.omar.77@hotmail.com>
2018-08-08 10:27:13 +0200

    first commit

---
+++ Pages/m7md osama/sidescript.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".toggler").click(function () {
+        $(".content").toggleClass("open-content");
+        $(".sidebar").toggleClass("open-sidebar");
+        $(".wrap").toggleClass("open-wrap");
+        $(".btn1").toggleClass("btn1h1");
+        $(".btn2").toggleClass("btn2h2");
+        $(".btn3").toggleClass("btn3h3");
+        $(".button").toggleClass("open-button");
+        $(".toggler").toggleClass("open-toggler");
+        
+        //$(".sidebar").click(function () {
+          //  $(".sidebar").removeClass("active");
+            //$(this).addClass('active');
+        //});
+    });
+
+    $('.html').localScroll();
+
+});
